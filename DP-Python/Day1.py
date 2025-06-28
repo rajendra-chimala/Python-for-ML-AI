31fde19fc80a2253f6676ac2a4f07351f8d250bd
rajendra-chimala <rajendrachimala5@gmail.com>
2025-06-28 23:19:16 +0545

    Commit

--- DP-Python/Day1.py
+++ DP-Python/Day1.py
@@ -1,19 +1,47 @@
-print("Hello World !")
-# name = "Rajendra Chimala"
-# age = 19
-# height = 5.7
+# print("Hello World !")
+# # name = "Rajendra Chimala"
+# # age = 19
+# # height = 5.7
 
-name= input("Enter Your Name : ")
-age = int(input("Enter your Age : "))
-height = float(input("Enter Your Height : "))
+# name= input("Enter Your Name : ")
+# age = int(input("Enter your Age : "))
+# height = float(input("Enter Your Height : "))
+# isAdult = bool(input("Enter boll : "))
 
-if(age>=18):
-    isAdult = True
-else:
-    isAdult = False
+# # if(age>=18):
+# #     isAdult = True
+# # else:
+# #     isAdult = False
 
-print(name," Type : ",type(name))
-print(age," Type : ",type(age))
-print(height," Type : ",type(height))
-print(isAdult," Type : ",type(isAdult))
+# print(name," Type : ",type(name))
+# print(age," Type : ",type(age))
+# print(height," Type : ",type(height))
+# print(isAdult," Type : ",type(isAdult))
+# print("hello","world")
 
+
+
+# Sepration 
+# print("Hello","World",sep="$")
+# print("Hello\n","World")
+# print(f"Hello World")
+
+# F String
+
+# num =10
+# print('the number is {}' .format(num))
+# print(F'the number is {num}')
+
+# num1 = 2.329543534
+
+# print(f"The Number is {num1:.3f}")
+
+num1= float(input("Enter a number :"))
+num2= float(input("Enter a number :"))
+Sum = num1+num2
+diff = num1-num2
+pro = num1*num2
+
+print("The sum is ",Sum)
+print("The Diff is ",diff)
+print("The Product is ",pro)
---
+++ DP-Python/Day2.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a Number : "))
+
+if(num<0):
+    print("Number is Negative !")
+elif(num==0):
+    print("Number is Zero !")
+else:
+    print("Number is Positive !")
