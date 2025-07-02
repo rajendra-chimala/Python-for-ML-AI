01c6f146d63825e831d2b2d93f67f019a4ab7097
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-02 20:11:02 +0545

    Commit

---
+++ DP-Python/Day4.py
@@ -0,0 +1,44 @@
+# List 
+# Tuples
+# Sets
+# Python Dictionaries
+
+# ls = [1,2,3,4]
+
+# print(ls)
+# print(type(ls))
+
+
+# x = [1,2,3,4,5,6,7,8,9]
+
+# x_sq = [a ** 2 for a in x]
+# print(x_sq)
+
+l3 = ["Samriddha", "Ram", 32, True, False, 3.2, 4.1, "Ramu", "Shyam", "Red Bull", 3.5, "Gita", "TensorFlow", "NumPy"]
+
+# print(l3[-1])
+# print(l3[-2])
+# print(l3[4])
+
+
+# Slicing list[starting_index:ending_index]
+
+# print(l3[0:])
+# print(l3[-1:0:-1])
+
+# print(l3[::-1])
+# print(l3[-3:])
+
+
+l1 = []
+
+l1.append("Rajendra ")
+l1.append("Binod")
+l1.append("Bikash")
+
+
+l1.insert(0,"Hari")
+
+print(l1)
+
+
