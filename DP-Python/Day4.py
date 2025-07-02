00154e776f86becdfad82c5ab16cd61de1300ca0
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-02 21:14:56 +0545

    commit

--- DP-Python/Day4.py
+++ DP-Python/Day4.py
@@ -30,15 +30,78 @@ l3 = ["Samriddha", "Ram", 32, True, False, 3.2, 4.1, "Ramu", "Shyam", "Red Bull"
 # print(l3[-3:])
 
 
-l1 = []
+# l1 = []
 
-l1.append("Rajendra ")
-l1.append("Binod")
-l1.append("Bikash")
-
-
-l1.insert(0,"Hari")
-
-print(l1)
+# l1.append("Rajendra ")
+# l1.append("Binod")
+# l1.append("Bikash")
 
 
+# l1.insert(0,"Hari")
+
+# print(l1)
+
+
+# RollNo = [23,42,5,23,45,12,97,12,34,10]
+
+# RollNo.sort()
+
+# print(RollNo)
+
+# POP in List 
+
+# ls = [1,2,3,4,5,6,7]
+
+# lelement = ls.pop()
+
+# print(lelement)
+
+# print(ls)
+
+
+# Tuple 
+
+
+mt = 1,2,3,4,5,1,1
+# print(type(mt))
+
+# print(mt.count(1))
+
+# index5 = mt.index(5)
+
+# print(index5)
+
+# # myt = (1)
+# myt = (1,)
+# print(type(myt))
+
+
+# without parenthesis tuples 
+
+# myt = 1,2,3,4,5
+# print(type(myt))
+
+
+# ----------------------------------------------------------------------------------
+
+# Set in Python
+
+
+mySet = {1,2,7,3,4,-2,5,0,-1,-5}
+
+# print(type(mySet))
+
+
+# Add a element 
+# mySet.add(6)
+
+# s = ()
+
+# print(mySet)
+
+
+# Empty Set 
+
+my_set  = set()
+
+print(type(my_set))
