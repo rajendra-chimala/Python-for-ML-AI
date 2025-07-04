4e9725755f56de45aef090cc2c1488200d61d121
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-04 21:22:06 +0545

    commit

--- DP-Python/Day6.py
+++ DP-Python/Day6.py
@@ -27,11 +27,41 @@
 # Using Break
 
 
-count = 0
-while count<10:
-    if count == 5:
-        break
-    print(count)    
-    count += 1  
+# count = 1
+# while count<10:
+#     if count == 5:
+#         break
+#     print(count)    
+#     count += 1  
     
+
+
+# Continue Example
+
+# count = 0
+# skipValue = 0
+# while count < 10:
+#     count += 1
+#     if count == 5:
+#         skipValue= 5
+#         continue 
+#     print(count)  
+# print(f"Skipped value: {skipValue}")  
+
+
+# skipped = []
+# for i in range(1, 16): # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+#     if i in [5, 6, 7] :
+#         skipped.append(i)
+#         continue
+#     print(i, end = " ")
+# print()
+# print(f"Skipped values = {skipped}")
+
+# -----------------------------------------------------------------------------------------------
+#Pass 
+# -----------------------------------------------------------------------------------------------
+# Nested Loop 
+ 
+
 
