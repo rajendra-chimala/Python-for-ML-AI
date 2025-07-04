57178022431cc750506897e3b0fcad4c1ad3f10e
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-04 20:06:56 +0545

    commit

---
+++ DP-Python/Day6.py
@@ -0,0 +1,37 @@
+# Loop through a list
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(" for loop:", fruit)
+
+
+
+# Printing numbers from 0 to 4 using a for loop
+# for i in range(5):
+#     print(i)
+    
+    
+# fruits = ["Apple", "Mango", "Banana"]
+# count = 0
+# for fruit in fruits:
+#     count = count + 1
+#     print(f"{count} : {fruit}")
+
+# Using While Loop
+
+# count = 0 ;
+# while count < 5:
+#     print(count)
+#     count += 1  # Increment count by 1
+        
+        
+# Using Break
+
+
+count = 0
+while count<10:
+    if count == 5:
+        break
+    print(count)    
+    count += 1  
+    
+
