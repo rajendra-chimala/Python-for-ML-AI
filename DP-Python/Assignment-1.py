e58af7a609a37bf0c17815b9b0542f5818f770e4
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-05 20:10:58 +0545

    commit

---
+++ DP-Python/Assignment-1.py
@@ -0,0 +1,134 @@
+# myList = ["hello world", "good morning", "python programming", "data science"]
+
+
+
+
+# result = [ml.title() for ml in myList] 
+# print("Data After Title Case:")
+# for item in result:
+#     print(item)
+
+
+
+
+
+
+
+
+# 9
+# data = {"name": "Rajendra    ", "city": "Dhanghadhi   ", "country": "Nepal   ","age": "25   "}
+# cleanedData = {k: v.rstrip() for k, v in data.items()} 
+# print("Data After Cleaning:")
+# for key, value in cleanedData.items():
+#     print(f"{key}: {value}")
+
+
+
+
+
+# 8
+# rawNames = ["rajendra","binod","suresh","karthik","ram","sita","gita","sanjay"]
+
+# capitalizeData = [name.capitalize() for name in rawNames]
+# print("Names After Capitalization:")
+# for name in capitalizeData:
+#     print(name) 
+
+
+
+
+
+
+
+
+
+
+#7 
+# text = " *$!#  Rajendra Chimala    **$$!! "
+
+# cleanText = text.strip("*$!# ")
+
+# print(f"Data After Cleaning :  {cleanText}")
+
+
+
+
+
+#6
+# fruitNames = [" Apple        ", "    Banana    ", " Mango", "  Pomegranet   ", "  Pineapple   "]
+
+# cleanedNames = [fName.strip() for fName in fruitNames]
+
+# print("Cleaned Fruit Names:")
+# for name in cleanedNames:
+#     print(name)
+
+
+
+
+
+
+
+
+
+
+# 5
+# fullName = "rajendra chimala"
+
+# TitledData = fullName.title()
+# print(f"Data Before Title Case :  {fullName}")
+# print(f"Data After Title Case :  {TitledData}")
+
+
+
+
+
+
+# 4
+# text = "he is one of the best programmers in the world."
+
+# capitalizeData = text.capitalize()
+
+# print(f"Data Before Capitalization :  {text}")
+# print(f"Data After Capitalization :  {capitalizeData}")
+
+
+
+
+
+
+
+# 3
+# text  = "Rajenrda Chimala**$$!!"
+# cleanData = text.rstrip("**$$!!")
+
+# print(f"Data Before Cleaning :  {text}")
+# print(f"Data After Cleaning :  {cleanData}")
+
+
+
+
+
+# 2
+# text = "*****###Rajendra Chimala"
+# cleanData = text.lstrip("*#")
+
+# print(f"Data Before Cleaning :  {text}")
+# print(f"Data After Cleaning :  {cleanData}")
+
+
+
+
+
+
+
+
+
+# 1
+# text = "   Rajendra Chimala     "
+# cleanData = text.strip()
+
+# print(f"Data Before Cleaning :  {text}")
+# print(f"Data After Cleaning :  {cleanData}")
+
+
