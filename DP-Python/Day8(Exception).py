7fd7472ec948e33e749ff2976ebfe522deb6dee3
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-07 20:39:31 +0545

    commit

--- DP-Python/Day8(Exception).py
+++ DP-Python/Day8(Exception).py
@@ -1,12 +1,81 @@
 # Exception Handling
 
-while True:
-            try:
-                x = int(input("Enter a number: "))
-                print(f"number = {x}")
 
-            except ValueError:
-                              print("Please Enter an Integer!!")
+# Value Error
+# while True:
+#             try:
+#                 x = int(input("Enter a number: "))
+#                 print(f"number = {x}")
+
+#             except ValueError:
+#                               print("Please Enter an Integer!!")
     
+#             else:
+#                  break
+
+
+
+# Zero Division Error
+
+# while True:
+#     try:
+#         x = int(input("Enter a number [?]/[] : "))
+#         y = int(input("Enter another number []/[?] : "))
+#         result = x / y
+#     except ZeroDivisionError:
+#         print("Division by zero error. : ")
+#         print(ZeroDivisionError.e)
+#     except ValueError:
+#         print("Please enter valid value.")
+#     else:
+#         print(f"Result [{x}]/[{y}] : {result}")
+#         break
+
+def calc(x,y,z):
+    while True:
+    
+        try:
+            res = (x+y)/z
+        except (ValueError,ZeroDivisionError) as x:
+            print(x)
+        else:
+            print(f"Result: {res}")
+            break
+        
+
+def calcs(x,y,z):
+    while True:
+            try:
+                res = x+y+z
+            except (ValueError,ZeroDivisionError) as x:
+                print(x)
             else:
-                 break
+                print(f"Result: {res}")
+                break
+
+
+def calcm(x,y,z):
+    while True:
+            try:
+                res = x*y*z
+            except (ValueError,ZeroDivisionError) as x:
+                print(x)
+            else:
+                print(f"Result: {res}")
+                break
+def main():
+    myList = input("Enter any numbers [1 2 3]: ").split()
+    op = input("Enter your operation \n[1] (x+y)/z \n[2] x+y+z \n[3] x*y*z : ")
+    
+    if op == '1':
+        calc(int(myList[0]), int(myList[1]), int(myList[2]))
+    elif op == '2':
+        calcs(int(myList[0]), int(myList[1]), int(myList[2]))
+    elif op == '3':
+        calcm(int(myList[0]), int(myList[1]), int(myList[2]))
+    else:
+        print("operation is not valid .")
+    
+    
+    
+main()
