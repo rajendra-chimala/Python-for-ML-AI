4f3513cdda65568ea3aceeedae3a3079faf25e9d
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-10 21:28:13 +0545

    commit

--- DP-Python/Day9(Maths D1).py
+++ DP-Python/Day9(Maths D1).py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.linalg as la
 
 # Defining vector
 # v = np.array([1, 2, 3, 4, 5]) 
@@ -19,8 +20,8 @@ import numpy as np
 # B= np.array([[1,2,3],[4,5,6],[7,8,9]])
 # C = np.array([[1,3],[3,6]])
 
-# mul = B*C
-# print("Matrix Multiplication:\n", mul)
+# = B*C
+# print("Matrix Multiplication:\n", )
 
 
 
@@ -44,12 +45,12 @@ import numpy as np
 # M2 = M1.T
 # print("Transpose of the Matrix:\n", M2)
 
-# Solve 2x + 3y = 8 and 5x + 4y = 13 using linear algebra using numpy.
+# Solve 2x + 3y = 8 and 5x + 4y = 13 using linear algebra using .
 
 # A = np.array([[2,3],[5,4]])
 # b = np.array([8,13])
 
-# solution = np.linalg.solve(A,b)
+solution = np.linalg.solve(A,b)
 
 # print("Solution of the equations:\n", solution)
 
@@ -61,8 +62,35 @@ import numpy as np
 
 # Answer 
 
-A = np.array([[8,3,-2], [-4,7,5],[3,4,-12]])
-b = np.array([9,15,35])
+# A = np.array([[8,3,-2], [-4,7,5],[3,4,-12]])
+# b = np.array([9,15,35])
 sol =np.linalg.solve(A,b)
 
-print(sol)
+# print("X = ", sol[0],"\nY = ", sol[1], "\nZ = ", sol[2])
+
+
+# LU Decomposition
+
+A = np.array([[2,4,5 ], [1,3,2], [4,2,1]])
+
+P, L, U = la.lu(A)
+
+# print("Matrix A:\n", A)
+# print("Permutation Matrix P:\n", P) 
+# print("Lower Triangular Matrix L:\n", L)
+# print("Upper Triangular Matrix U:\n", U)
+
+# P x A = L x U
+
+# PA = P@A
+# LU = L@U
+
+# print("P x A:\n", PA)
+# print("L x U:\n", LU)
+
+
+# QR decomposition
+A = np.array([[1,2,3],[3,4,5]])
+Q,R = la.qr(A)
+
+# 
