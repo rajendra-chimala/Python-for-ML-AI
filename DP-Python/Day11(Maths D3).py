0a5d4e7f1729db4f929542fa92b1a0251500fa3f
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-12 21:16:45 +0545

    commit

---
+++ DP-Python/Day11(Maths D3).py
@@ -0,0 +1,31 @@
+#Basis
+# import numpy as np
+
+# v1 = np . array ([1 , 0 , 0])
+# v2 = np . array ([0 , 1 , 0])
+# v3 = np . array ([0 , 0 , 1])
+
+# A = np . column_stack (( v1 , v2 , v3 ) )
+
+
+# Check if the matrix is full rank (i.e. , vectors are linearl
+# print ( np . linalg . matrix_rank ( A ) ) 
+
+# import numpy as np
+# v1 = np . array ([1 , 0])
+# v2 = np . array ([1 , 1])
+# A = np . column_stack (( v1 , v2 ) )
+# print ( np . linalg . matrix_rank ( A ) )
+
+
+# # Rank of a Matrix
+
+# import numpy as np 
+
+# v1 = np.array([1, 2, 3])
+# v2 = np.array([2, 4, 6])
+# v3 = np.array([9, 8, 10])
+
+# A = np.column_stack((v1, v2, v3))
+# Rank = np.linalg.matrix_rank(A)
+# print(f"Rank of the matrix  = {Rank}")
