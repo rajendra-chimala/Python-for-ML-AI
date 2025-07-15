6aa541d86cb8f11483d8c00ba0a4c25551b53344
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-15 19:55:01 +0545

    commit

---
+++ DP-Python/Day12.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Array Attributes
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+print("Array:\n", arr)
+
+# Shape of the array
+print("Shape:", arr.shape)
+
+# Size of the array
+print("Size of Array :\n", arr.size)
