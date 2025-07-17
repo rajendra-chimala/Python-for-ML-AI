43987cf7dae29fbc335eda97dc422f58756ae15e
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-17 20:23:59 +0545

    commit

--- DP-Python/Day13.py
+++ DP-Python/Day13.py
@@ -91,24 +91,57 @@ B = np.array([[6,7], [8,9]])
 
 # Matrix Transpose
 
-transposed_A = A.T
-print("Transposed Matrix A:\n", transposed_A)
+# transposed_A = A.T
+# print("Transposed Matrix A:\n", transposed_A)
 
 
-# Diterminant of a Matrix
-det_of_A = np.linalg.det(A)
-print("Determinant of Matrix A:", det_of_A)
+# # Diterminant of a Matrix
+# det_of_A = np.linalg.det(A)
+# print("Determinant of Matrix A:", det_of_A)
 
 
-# Inverse of a Matrix
-inv_A = np.linalg.inv(A)
-print("Inverse of Matrix A:\n", inv_A)
+# # Inverse of a Matrix
+# inv_A = np.linalg.inv(A)
+# print("Inverse of Matrix A:\n", inv_A)
 
 
-# Eiganvalues and Eigenvectors
-eigenvalues, eigenvectors = np.linalg.eig(A)
-print("Eigenvalues of Matrix A:", eigenvalues)
-print("Eigenvectors of Matrix A:\n", eigenvectors)
+# # Eiganvalues and Eigenvectors
+# eigenvalues, eigenvectors = np.linalg.eig(A)
+# print("Eigenvalues of Matrix A:", eigenvalues)
+# print("Eigenvectors of Matrix A:\n", eigenvectors)
 
 
-# 
+#############################################################################################
+# Advacned Mathematical Functions
+
+# arr = np.array([1, 2, 3, 4,1,3, 5,2,2])
+
+# # Sum of all elements
+# sum_of_elements = np.sum(arr)
+# print("Sum of all elements:", sum_of_elements) 
+
+# cumulative_sum = np.cumsum(arr)
+# print("Cumulative Sum of elements:", cumulative_sum)
+
+
+# # Finding Unique Elements
+
+# unique = np.unique(arr)
+# unique,count = np.unique(arr,return_counts=True)
+# print("Unique elements in the array:", unique)
+
+# print("Count of unique elements:", count)
+
+
+# # Sorting Arrays
+# sorted_arr = np.sort(arr)
+# asorted_arr = np.argsort(arr)
+# print("Sorted Array:", sorted_arr)
+# print("Indices of sorted elements:", asorted_arr)
+
+
+
+data = np.array([10, 20, 30, 40, 50])
+print("25th percentile:", np.percentile(data, 25))
+print("50th percentile (median):", np.percentile(data, 50))
+print("75th percentile:", np.percentile(data, 75))
