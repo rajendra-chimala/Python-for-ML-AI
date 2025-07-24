beac5a28847eff9b45b69aeeec02c1f53fe554a8
rajendra-chimala <rajendrachimala5@gmail.com>
2025-07-24 16:01:55 +0545

    commit

---
+++ DP-Python/OCV.py
@@ -0,0 +1,44 @@
+import cv2
+
+
+image = cv2.imread('image.png')
+
+if image is not None:
+    print("Image loaded successfully !")
+    # print(image)
+    # print(image.dtype)
+    # cv2.imshow("This is Image !",image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    
+    
+# else:
+#     print("Failed to load image.")
+    
+    
+# isSaved = cv2.imwrite("output.png",image)
+# if isSaved:
+#     print("Image Successfully !")
+# else:
+#     print("Faild to save an image !"    )
+    
+oimg = cv2.imread("output.png")
+# cv2.imshow("This is Image !",oimg)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+# Dimensiotion 
+
+# h,w,c = oimg.shape
+
+# print(f"Height : {h}, Width : {w}, Channels : {c}")
+
+# Gray Scale converting 
+
+gray_scale = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+cv2.imshow("This is Image !",gray_scale)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+    
