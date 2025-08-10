8744a65dfadd1676b89cbfcaa43adeaf0b83a29f
rajendra-chimala <rajendrachimala5@gmail.com>
2025-08-10 19:33:02 +0545

    commit

---
+++ DP-Python/Day27 (Skin Detection).py
@@ -0,0 +1,11 @@
+# Algorithm for skin detection using OpenCV
+
+# Start
+# Load the image
+# Convert it into the HSV color space
+# Define the HSV range for different range of skin colors
+# Create the mask using the defined range
+# Display the original image and the mask
+# Stop 
+
+
