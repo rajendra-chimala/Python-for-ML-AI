30f6bdf8cd1aae45e9de4325d7032d99dcbfea70
rajendra-chimala <rajendrachimala5@gmail.com>
2025-08-11 21:07:02 +0545

    commit

--- DP-Python/Day27 (Skin Detection).py
+++ DP-Python/Day27 (Skin Detection).py
@@ -9,3 +9,22 @@
 # Stop 
 
 
+import numpy as np
+import cv2 as cv
+
+image = cv.imread('img/MZ.jpg')
+
+hsv_image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+lower_skin = np.array([0, 20, 70], dtype=np.uint8)
+upper_skin = np.array([20, 255, 255], dtype=np.uint8)
+
+
+mask = cv.inRange(hsv_image, lower_skin, upper_skin)
+
+skin = cv.bitwise_and(image,image, mask=mask)
+
+cv.imshow("Skin Detection", cv.resize(skin, (300, 400)))
+cv.imshow("Original Image", cv.resize(image, (300, 400)))
+cv.imshow("Skin Mask", cv.resize(mask, (300, 400)))
+cv.waitKey(0)
