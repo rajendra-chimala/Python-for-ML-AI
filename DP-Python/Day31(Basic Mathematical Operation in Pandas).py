28a66841321047dc6f0968269a1d5e3bc034f865
rajendra-chimala <rajendrachimala5@gmail.com>
2025-08-16 20:01:41 +0545

    Day-31

---
+++ DP-Python/Day31(Basic Mathematical Operation in Pandas).py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+data = {
+"A": [10 , 20 , 30 , 40] ,
+"B": [5 , 15 , 25 , 35] ,
+"C": [2 , 4 , 6 , 8]
+}
+
+
+
+df = pd.DataFrame(data)
+
+# print(df)
+
+
+# 4 Basic Arithmetic Operations
+
+# df["Sum of A and B"] = df["A"]+df["B"]
+
+# print("Sum of A and B:")
+# print(df["Sum of A and B"])
+
+
+# df["Subtraction of A and B"] = df["A"]-df["B"]
+# print("\nSubtraction of A and B:")
+# print(df["Subtraction of A and B"])
+
+
+# df["Multiplication of A and B"] = df["A"]*df["B"]
+# print("\nMultiplication of A and B:")
+# print(df["Multiplication of A and B"])
+
+
+# df["Division of A and B"] = df["A"]/df["B"]
+# print("\nDivision of A and B:")
+# print(df["Division of A and B"])
+
+
+
+# Descriptive Statistics
+
+
+print()
