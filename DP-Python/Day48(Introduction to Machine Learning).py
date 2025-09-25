a47d86ab961136124f65ff5921fccd4ff6427bce
rajendra-chimala <rajendrachimala5@gmail.com>
2025-09-25 21:26:25 +0545

    commit

--- DP-Python/Day48(Introduction to Machine Learning).py
+++ DP-Python/Day48(Introduction to Machine Learning).py
@@ -1,1 +1,12 @@
-#  
+# #Day49
+# Continuing my AI & Machine Learning journey with Digital Pathshala, under the mentorship of Sir Samriddha Pathak, today I explored types of Machine Learning in depth.
+# 🔹 Key concepts covered:
+# ✅ Supervised Learning – learning with labeled data.
+# ✅ Unsupervised Learning – finding hidden patterns without labels.
+# ✅ Semi-Supervised Learning – mix of labeled & unlabeled data.
+# ✅ Reinforcement Learning – learning by trial and reward.
+# ✅ Built a small label-based ML model using Teachable Machine.
+
+# 💡 Key takeaway: Each ML type has its unique strengths—choosing the right one depends on the problem and the data available.
+
+# #AI #MachineLearning #DataScience #SupervisedLearning #UnsupervisedLearning #ReinforcementLearning #TeachableMachine #DigitalPathshala #Day49
